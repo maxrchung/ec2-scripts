47afd3796a5ac4f33cf9c283bd7469181ec46679
EC2 Default User <ec2-user@ip-10-0-17-80.us-west-1.compute.internal>
2022-01-06 05:37:12 +0000

    add mongo backup

---
+++ backup-mongo.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+while getopts u:p:d: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        p) password=${OPTARG};;
+        d) database=${OPTARG};;
+    esac
+done
+
+file_name=`date +%Y%m%d_%H%M%S`.dump
+mongodump --username=$username --password=$password --db=$database --archive=$file_name --authenticationDatabase=admin
+bzip2 $file_name
+
+aws s3 cp $file_name.bz2 s3://maxrchung-database-backup/$database/$file_name --storage-class ONEZONE_IA
+
+rm $file_name.bz2
