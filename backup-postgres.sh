c2aee17a0d0d09c8a4bdd66b9bb744ae17b0ad7d
Max <maxrchung@gmail.com>
2022-01-12 01:30:32 -0800

    Update backup-postgres.sh

--- backup-postgres.sh
+++ backup-postgres.sh
@@ -17,6 +17,6 @@ PGPASSWORD=$password pg_dump -U $username -d $database -f $file_name
 bzip2 $file_name
 
 # Glacier requires a minimum storage duration of 90+ days, so next cheapest option is one zone infrequent access
-aws s3 cp $file_name.bz2 s3://maxrchung-database-backup/$database/$file_name --storage-class ONEZONE_IA
+aws s3 cp $file_name.bz2 s3://maxrchung-database-backup/$database/$file_name.bz2 --storage-class ONEZONE_IA
 
 rm $file_name.bz2
